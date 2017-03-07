267bf1fdd1e1c75cdd2650c18e1c54728e97c0f7
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-08 01:05:50 +0800

    Add conf

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 for batch_size in 500; do
-	for trainset_rate in 0.9 0.1; do
-		for lr in 0.0001 0.00005; do
+	for trainset_rate in 0.8 0.2 0.7 0.3 0.6 0.4 0.5; do
+		for lr in 0.0001; do
 			for dim in 100;do #100>75
 				for reg in 0.03; do
 					for seed in 0 1 2 3 4; do
