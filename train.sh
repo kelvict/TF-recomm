99cdfa66de63c3327ea94001d0a46f354abe05a0
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-07 18:41:42 +0800

    Add conf

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ for batch_size in 500; do
 	for trainset_rate in 0.9; do
 		for lr in 0.0001; do
 			for dim in 100;do #100>75
-				for reg in 0.03 0.04; do
+				for reg in 0.03 0.04 0.02; do
 					for seed in 0 1 2; do
 					python svd_train_val.py ${seed} ${trainset_rate} ${lr} ${reg} ${dim} ${batch_size}> log/biasMf_${batch_size}_${seed}_${trainset_rate}_${lr}_${reg}_${dim}.log 2>&1 &
 					done
